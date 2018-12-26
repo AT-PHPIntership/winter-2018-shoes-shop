a1bda6c101a5607d35d6010732401d55cdbbbd3b
thanhbuiv <thanh.bui@asiantech.vn>
2018-12-26 22:16:23 +0700

    create migration, seeder, factory, model for tbl Users

---
+++ app/Models/User.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Notifications\Notifiable;
+use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class User extends Authenticatable
+{
+    use Notifiable, SoftDeletes;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'name', 'email', 'password',
+    ];
+
+    /**
+     * The attributes that should be hidden for arrays.
+     *
+     * @var array
+     */
+    protected $hidden = [
+        'password', 'remember_token',
+    ];
+
+    /**
+     * The attributes that should be mutated to dates.
+     *
+     * @var array
+     */
+    protected $dates = ['deleted_at'];
+
+    /**
+     * Get Profile Object
+     *
+     * @return object
+     */
+    public function profile()
+    {
+        return $this->hasOne('App\Models\Profile');
+    }
+    
+    /**
+     * Get Role Object
+     *
+     * @return object
+     */
+    public function role()
+    {
+        return $this->belongsTo('App\Models\Role');
+    }
+}
