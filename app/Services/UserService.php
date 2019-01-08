2d809f220c705468953f7387900cd16424c52c04
thanhbuiv <thanh.bui@asiantech.vn>
2019-01-08 09:31:38 +0700

    edit UserService

--- app/Services/UserService.php
+++ app/Services/UserService.php
@@ -33,7 +33,7 @@ class UserService
     {
         DB::beginTransaction();
         try {
-            if ($data['role_id'] === Role::ADMIN_ROLE && !isAdminLogin()) {
+            if ($data['role_id'] == Role::ADMIN_ROLE && !isAdminLogin()) {
                 return false;
             }
             $user = User::create([
@@ -95,7 +95,7 @@ class UserService
     {
         DB::beginTransaction();
         try {
-            if ($data['role_id'] === Role::ADMIN_ROLE && !isAdminLogin()) {
+            if ($data['role_id'] == Role::ADMIN_ROLE && !isAdminLogin()) {
                 return false;
             }
             $inputUser = [
