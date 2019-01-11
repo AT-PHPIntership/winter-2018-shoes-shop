5e43dde02db56f2143232e4fd1e6e5428c1805ac
thanhbuiv <thanh.bui@asiantech.vn>
2019-01-11 10:32:27 +0700

    create ui list promotion

---
+++ app/Http/Controllers/Admin/PromotionController.php
@@ -0,0 +1,18 @@
+<?php
+namespace App\Http\Controllers\Admin;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Admin\Controller;
+
+class PromotionController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return view('admin.promotion.list');
+    }
+}
