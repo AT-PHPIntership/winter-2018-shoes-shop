5d4bd7fb511d6311cb305496a0c97d6b177f9e8b
thanhbuiv <thanh.bui@asiantech.vn>
2019-01-11 11:09:10 +0700

    handle list promotion

--- app/Http/Controllers/Admin/PromotionController.php
+++ app/Http/Controllers/Admin/PromotionController.php
@@ -3,9 +3,24 @@ namespace App\Http\Controllers\Admin;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\Admin\Controller;
+use App\Services\PromotionService;
 
 class PromotionController extends Controller
 {
+    private $promotionService;
+
+    /**
+    * Contructer
+    *
+    * @param App\Service\PromotionService $promotionService promotionService
+    *
+    * @return void
+    */
+    public function __construct(PromotionService $promotionService)
+    {
+        $this->promotionService = $promotionService;
+    }
+
     /**
      * Display a listing of the resource.
      *
@@ -13,6 +28,7 @@ class PromotionController extends Controller
      */
     public function index()
     {
-        return view('admin.promotion.list');
+        $promotions = $this->promotionService->getPromotionWithPaginate();
+        return view('admin.promotion.list', compact('promotions'));
     }
 }
--- config/define.php
+++ config/define.php
@@ -4,6 +4,10 @@ return [
     'paginate' => [
         'limit_rows' => 10,
     ],
+    'orderBy' => [
+        'desc' => 'desc',
+    ],
+    'date_vn' => 'd-m-Y',
     'path' => [
         'avatar' => '/upload/',
         'default_avatar' => '/admin/images/default_avatar.png',
