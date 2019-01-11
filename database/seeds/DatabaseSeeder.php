5b59cd922b038e6894b19b16ff8d6bdaab3b087e
thanhbuiv <thanh.bui@asiantech.vn>
2019-01-11 09:38:31 +0700

    model promotion

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -22,6 +22,7 @@ class DatabaseSeeder extends Seeder
         $this->call(ProductDetailsTableSeeder::class);
         $this->call(ImagesTableSeeder::class);
         $this->call(PromotionsTableSeeder::class);
+        $this->call(ProductPromotionsTableSeeder::class);
         DB::statement('SET FOREIGN_KEY_CHECKS=1;');
     }
 }
