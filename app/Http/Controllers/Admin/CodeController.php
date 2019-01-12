4ef46b6e6a2cdda0e574f3f4589aa5146de8b97d
thanhbuiv <thanh.bui@asiantech.vn>
2019-01-12 15:14:05 +0700

    create ui edit code v2

--- app/Http/Controllers/Admin/CodeController.php
+++ app/Http/Controllers/Admin/CodeController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Admin;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Admin\Controller;
 use App\Services\CodeService;
+use App\Models\Code;
 
 class CodeController extends Controller
 {
@@ -31,5 +32,17 @@ class CodeController extends Controller
     {
         $codes = $this->codeService->getCodeWithPaginate();
         return view('admin.code.list', compact('codes'));
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param App\Models\Code $code code
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Code $code)
+    {
+        return view('admin.code.edit', compact('code'));
     }
 }
---
+++ resources/views/admin/code/edit.blade.php
@@ -0,0 +1,90 @@
+@extends('admin.module.master')
+@section('content')
+  <div class="content-wrapper">
+    <section class="content-header">
+      <h1>
+          @lang('code.title')
+      </h1>
+    </section>
+    <section class="content">
+      <div class="row">
+        <div class="col-md-12">
+          @include('admin.module.message')
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-md-12">
+          <div class="box box-primary">
+            <div class="box-header with-border">
+              <h3 class="box-title">@lang('code.edit.title')</h3>
+            </div>
+            <form method="POST" role="form" action="">
+              @csrf
+              @method('PUT')
+              <div class="box-body">
+                <div class="form-group">
+                  <label for="exampleInputName">@lang('code.table.name') *</label>
+                  <input type="hidden" name="id" value="{{ $code->id }}">
+                  <input type="text" name="name" class="form-control" id="exampleInputName" value="{{ $code->name }}">
+                  @if ($errors->has('name'))
+                    <span class="help-block">{{ $errors->first('name') }}</span>
+                  @endif
+                </div>
+                <div class="form-group">
+                  <label for="exampleInputCategory">@lang('code.table.category') *</label>
+                  <select name="category_id" class="form-control" id="exampleInputCategory">
+                    <option value="" {{ $code->category_id == null ? "selected": "" }}>@lang('code.null')</option>
+                    @foreach ($categories as $category)
+                      <option value="{{ $category->id }}" {{ $code->category_id == $category->id ? "selected": "" }}>{{ $category->name }}</option>
+                    @endforeach
+                  </select>
+                  @if ($errors->has('category_id'))
+                    <span class="help-block">{{ $errors->first('category_id') }}</span>
+                  @endif
+                </div>
+                <div class="form-group">
+                  <label for="exampleInputPercent">@lang('code.table.percent') *</label>
+                  <input type="number" name="percent" class="form-control" id="exampleInputPercent" value="{{ $code->percent }}">
+                  @if ($errors->has('percent'))
+                    <span class="help-block">{{ $errors->first('percent') }}</span>
+                  @endif
+                </div>
+                <div class="form-group">
+                  <label for="exampleInputDescription">@lang('code.table.description')</label>
+                  <input type="text" name="description" class="form-control" id="exampleInputDescription" value="{{ $code->description }}">
+                  @if ($errors->has('description'))
+                    <span class="help-block">{{ $errors->first('description') }}</span>
+                  @endif
+                </div>
+                <div class="form-group">
+                  <label for="exampleInputTimes">@lang('code.table.times') *</label>
+                  <input type="number" name="times" class="form-control" id="exampleInputTimes" value="{{ $code->times }}">
+                  @if ($errors->has('times'))
+                    <span class="help-block">{{ $errors->first('times') }}</span>
+                  @endif
+                </div>
+                <div class="form-group">
+                  <label for="exampleInputStart_date">@lang('code.table.start_date') *</label>
+                  <input type="date" name="start_date" class="form-control" id="exampleInputStart_date" value="{{ $code->start_date }}">
+                  @if ($errors->has('start_date'))
+                    <span class="help-block">{{ $errors->first('start_date') }}</span>
+                  @endif
+                </div>
+                <div class="form-group">
+                  <label for="exampleInputEnd_date">@lang('code.table.end_date') *</label>
+                  <input type="date" name="end_date" class="form-control" id="exampleInputEnd_date" value="{{ $code->end_date }}">
+                  @if ($errors->has('end_date'))
+                    <span class="help-block">{{ $errors->first('end_date') }}</span>
+                  @endif
+                </div>
+              </div>
+              <div class="box-footer">
+                <button type="submit" class="btn btn-primary">@lang('common.edit')</button>
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
+    </section>
+  </div>
+@endsection
