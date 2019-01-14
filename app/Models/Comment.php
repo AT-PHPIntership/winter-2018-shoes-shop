713cb2fdd680793f61f656cd4264c74a731f95d0
thanhbuiv <thanh.bui@asiantech.vn>
2019-01-14 16:12:55 +0700

    model comment

---
+++ app/Models/Comment.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Comment extends Model
+{
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'user_id', 'product_id', 'parent_id', 'content',
+    ];
+    
+    /**
+     * Comment belong to User
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    /**
+     * Comment belong to Product
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function product()
+    {
+        return $this->belongsTo(Product::class);
+    }
+}
