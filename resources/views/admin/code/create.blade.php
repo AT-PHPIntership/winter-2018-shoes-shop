ea3a89d59131e813370b248dce447004ee1fc75e
thanhbuiv <thanh.bui@asiantech.vn>
2019-01-17 10:34:42 +0700

    check cmt review

--- resources/views/admin/code/create.blade.php
+++ resources/views/admin/code/create.blade.php
@@ -22,15 +22,15 @@
               @csrf
               <div class="box-body">
                 <div class="form-group">
-                  <label for="exampleInputName">@lang('code.table.name') *</label>
-                  <input type="text" name="name" class="form-control" id="exampleInputName" value="{{ old('name') }}">
+                  <label for="code-input-name">@lang('code.table.name') *</label>
+                  <input type="text" name="name" class="form-control" id="code-input-name" value="{{ old('name') }}">
                   @if ($errors->has('name'))
                     <span class="help-block">{{ $errors->first('name') }}</span>
                   @endif
                 </div>
                 <div class="form-group">
-                  <label for="exampleInputCategory">@lang('code.table.category') *</label>
-                  <select name="category_id" class="form-control" id="exampleInputCategory">
+                  <label for="code-slt-category">@lang('code.table.category') *</label>
+                  <select name="category_id" class="form-control" id="code-slt-category">
                     <option value="">@lang('code.null')</option>
                     @foreach ($categories as $category)
                       <option value="{{ $category->id }}" {{ old('category_id') === $category->id ? 'selected' : '' }}>{{ $category->name }}</option>
@@ -41,36 +41,36 @@
                   @endif
                 </div>
                 <div class="form-group">
-                  <label for="exampleInputPercent">@lang('code.table.percent') *</label>
-                  <input type="number" name="percent" class="form-control" id="exampleInputPercent" value="{{ old('percent') }}">
+                  <label for="code-input-percent">@lang('code.table.percent') *</label>
+                  <input type="number" name="percent" class="form-control" id="code-input-percent" value="{{ old('percent') }}">
                   @if ($errors->has('percent'))
                     <span class="help-block">{{ $errors->first('percent') }}</span>
                   @endif
                 </div>
                 <div class="form-group">
-                  <label for="exampleInputDescription">@lang('code.table.description')</label>
-                  <textarea class="form-control" name="description" id="exampleInputDescription">{{ old('description') }}</textarea>
+                  <label for="code-txt-description">@lang('code.table.description')</label>
+                  <textarea class="form-control" name="description" id="code-txt-description">{{ old('description') }}</textarea>
                   @if ($errors->has('description'))
                     <span class="help-block">{{ $errors->first('description') }}</span>
                   @endif
                 </div>
                 <div class="form-group">
-                  <label for="exampleInputTimes">@lang('code.table.times') *</label>
-                  <input type="number" name="times" class="form-control" id="exampleInputTimes" value="{{ old('times') }}">
+                  <label for="code-input-times">@lang('code.table.times') *</label>
+                  <input type="number" name="times" class="form-control" id="code-input-times" value="{{ old('times') }}">
                   @if ($errors->has('times'))
                     <span class="help-block">{{ $errors->first('times') }}</span>
                   @endif
                 </div>
                 <div class="form-group">
-                  <label for="exampleInputStart_date">@lang('code.table.start_date') *</label>
-                  <input type="date" name="start_date" class="form-control" id="exampleInputStart_date" value="{{ old('start_date') }}">
+                  <label for="code-input-startdate">@lang('code.table.start_date') *</label>
+                  <input type="date" name="start_date" class="form-control" id="code-input-startdate" value="{{ old('start_date') }}">
                   @if ($errors->has('start_date'))
                     <span class="help-block">{{ $errors->first('start_date') }}</span>
                   @endif
                 </div>
                 <div class="form-group">
-                  <label for="exampleInputEnd_date">@lang('code.table.end_date') *</label>
-                  <input type="date" name="end_date" class="form-control" id="exampleInputEnd_date" value="{{ old('end_date') }}">
+                  <label for="code-input-enddate">@lang('code.table.end_date') *</label>
+                  <input type="date" name="end_date" class="form-control" id="code-input-enddate" value="{{ old('end_date') }}">
                   @if ($errors->has('end_date'))
                     <span class="help-block">{{ $errors->first('end_date') }}</span>
                   @endif
