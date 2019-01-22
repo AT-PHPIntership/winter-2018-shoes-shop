5159767e5f3afc0d31ca4c960ac32d7ab53ddb89
thanhbuiv <thanh.bui@asiantech.vn>
2019-01-22 15:43:13 +0700

    modify ui add

--- resources/views/admin/promotion/create.blade.php
+++ resources/views/admin/promotion/create.blade.php
@@ -18,7 +18,7 @@
             <div class="box-header with-border">
               <h3 class="box-title">@lang('promotion.add.title')</h3>
             </div>
-            <form method="POST" role="form" action="{{ route('admin.promotions.store') }}">
+            <form method="POST" role="form" action="">
               @csrf
               <div class="box-body">
                 <div class="form-group">
@@ -44,14 +44,13 @@
                 </div>
                 <div class="form-group">
                   <label for="promotion-slt-product">@lang('promotion.table.product')</label>
-                  <select class="form-control select2" multiple="multiple" data-placeholder="@lang('promotion.select')"
-                          style="width: 100%;">
+                  <select name="product_id" class="form-control select2" multiple="multiple" data-placeholder="@lang('promotion.select')">
                     @foreach ($products as $product)
                       <option value="{{ $product->id }}">{{ $product->name }}</option>
                     @endforeach
                   </select>
-                  @if ($errors->has('product'))
-                    <span class="help-block">{{ $errors->first('product') }}</span>
+                  @if ($errors->has('product_id'))
+                    <span class="help-block">{{ $errors->first('product_id') }}</span>
                   @endif
                 </div>
                 <div class="form-group">
