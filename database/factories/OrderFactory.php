ff23d07f366523126d21aa25b27ffecc441e9688
thanhbuiv <thanh.bui@asiantech.vn>
2019-01-28 11:31:31 +0700

    check cmt 2

--- database/factories/OrderFactory.php
+++ database/factories/OrderFactory.php
@@ -10,6 +10,6 @@ $factory->define(Order::class, function (Faker $faker) {
         'shipping_address' => $faker->address,
         'phone_number' => '09'.$faker->randomNumber(8),
         'total_amount' => rand(100000, 1000000),
-        'status' => array_random([Order::ORDER_STATUS['CONFIRMED'], Order::ORDER_STATUS['PROCESSING'], Order::ORDER_STATUS['QUALITY_CHECK'], Order::ORDER_STATUS['DISPATCHED_ITEM'], Order::ORDER_STATUS['DELIVERED'], Order::ORDER_STATUS['CANCELED'], Order::ORDER_STATUS['PENDING']]),
+        'status' => array_random(Order::ORDER_STATUS),
     ];
 });
