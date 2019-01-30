8317e2010badbe113809fde423b564d7a4cef7c6
thanhbuiv <thanh.bui@asiantech.vn>
2019-01-30 11:38:44 +0700

    add lang

---
+++ resources/lang/vn/index.php
@@ -0,0 +1,46 @@
+<?php
+
+return [
+    'header' => [
+        'home' => 'Trang chủ',
+        'login' => 'Đăng nhập',
+        'search' => 'Tìm kiếm',
+        'contact' => [
+            'phone' => '0272.727.272',
+            'email' => 'shoesshop@asiantech.vn',
+        ],
+    ],
+    'footer' => [
+        'about_us' => [
+            'title' => 'Thông tin',
+            'content' => 'AsianTech',
+        ],
+        'news_letter' => [
+            'title' => 'Bản tin',
+            'content' => 'Cập nhật thông tin mới nhất',
+            'email' => 'Email',
+        ],
+        'instagram' => [
+            'title' => 'Instagram',
+        ],
+        'follow_us' => [
+            'title' => 'Theo dõi',
+        ],
+        'copy_right' => [
+            'title' => 'Copyright',
+            'content' => 'All rights reserved | This template is made with',
+            'by' => 'by ThanhBui - DungLe'
+        ],
+    ],
+    'category' => 'Danh mục',
+    'shose' => [
+        'men' => 'Giày nam',
+        'women' => 'Giày nữ',
+        'kid' => 'Giày trẻ em',
+        'accessories' => 'Phụ kiện',
+    ],
+    'product' => [
+        'new' => 'Sản phẩm mới',
+        'top_sell' => 'Sản phẩm bán chạy',
+    ],
+];
