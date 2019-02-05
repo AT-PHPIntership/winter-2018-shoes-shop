222627d74f91551d1dd1807dd4637315509e2607
thanhbuiv <thanh.bui@asiantech.vn>
2019-02-05 23:14:02 +0700

    fix popup

--- public/public/js/script.js
+++ public/public/js/script.js
@@ -1,26 +1,19 @@
-$('.js-addcart').each(function(){
-    var nameProduct = $(this).parent().parent().parent().find('.top .head').html();
-    $(this).on('click', function(){
-        swal(nameProduct, "is added to cart !", "success");
-        // alert(nameProduct);
-    });
-});
 $(document).ready(function(){
-    $(".owl-carousel").owlCarousel({
-        loop:true,
-        margin:10,
-        nav:true,
-        dots:false,
-        responsive:{
-            0:{
-                items:1
-            },
-            600:{
-                items:2
-            },
-            1000:{
-                items:4
-            }
-        }
-    });
+  $(".owl-carousel").owlCarousel({
+    loop:true,
+    margin:10,
+    nav:true,
+    dots:false,
+    responsive:{
+      0:{
+        items:1
+      },
+      600:{
+        items:2
+      },
+      1000:{
+        items:4
+      }
+    }
   });
+});
