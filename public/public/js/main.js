e05d8d2a9e444bb828deaf3387f2d71dec615d0e
thanhbuiv <thanh.bui@asiantech.vn>
2019-02-13 17:19:34 +0700

    add sticky

--- public/public/js/main.js
+++ public/public/js/main.js
@@ -1,5 +1,17 @@
 $(document).ready(function(){
-  // -------   Active Mobile Menu-----//
+  "use strict";
+
+  // Sticky
+	var window_width 	 = $(window).width(),
+	window_height 		 = window.innerHeight,
+	header_height 		 = $(".default-header").height(),
+	header_height_static = $(".site-header.static").outerHeight(),
+	fitscreen 			 = window_height - header_height;
+	$(".fullscreen").css("height", window_height)
+	$(".fitscreen").css("height", fitscreen);
+  $(".default-header").sticky({topSpacing:0});
+  
+  // Active Mobile Menu
   $(".navbar-nav li a[href^='#']").on('click', function(event) {
     var target = this.hash;
     event.preventDefault();
