efc4746cd79c916f3ade76c9dab1d44158ef3146
Asiantech <dung.le2@asiantech.vn>
2019-02-21 09:28:26 +0700

    [manage-order] doing

--- resources/views/user/pages/order/list.blade.php
+++ resources/views/user/pages/order/list.blade.php
@@ -1,5 +1,6 @@
 @extends('user.module.master')
 @section('content')
+@dd($orders[1]->orderDetails)
   <div class="content-wrapper p-t-50">
     @include('user.module.sidebar')
     <div class="content full-with">
@@ -7,48 +8,31 @@
         <h1 class="box-title text-uppercase">@lang('user.manage_order')</h1>
       </section>
       <section class="orders">
-				<div class="box-header with-border">
-					<h3 class="box-title">@lang('user.order_list')</h3>
-				</div>
-				<div class="box-body text-dark">
-					<div class="row item">
-						<div class="col-sm-3 item-info">
-							<p class="text-body">Đơn hàng <strong>#212</strong></p>
-							<p>Ngày đặt: 09/2/2018</p>
-							<p>Giá: 10000000Đ</p>
+					<h3>@lang('user.order_list')</h3>
+				<div class="text-dark">
+					@foreach ($orders as $item)
+					<div class="box">
+						<div class="box-header with-border">
+							<h5>Đơn hàng <strong class="order-num">#{{ $item->id}}</strong>Ngày đặt: {{ $item->created_at}}</h5>
 						</div>
-						<div class="col-sm-9 item-detail">
-							<ul class="product-menu">
-								<li class="row prodcut-item">
-									<img class="col-sm-4 product-img" alt="hình ảnh">
-									<span class="col-sm-5 item-padding">Thông tin sản phẩm</span>
-									<span class="col-sm-3 item-padding">Số lượng: 2</span>
-								</li>
-								<li class="row prodcut-item">
-										<img class="col-sm-4 product-img" alt="hình ảnh">
+						<div class="box-body">
+							<div class="col-sm-9 item-detail">
+								<ul class="product-menu">
+									<li class="row prodcut-item">
+									<img class="col-sm-4 product-img" src="{{$item->orderDetails->product->}}" alt="hình ảnh">
 										<span class="col-sm-5 item-padding">Thông tin sản phẩm</span>
-										<span class="col-sm-3 item-padding">Số lượng: 1</span>
+										<span class="col-sm-3 item-padding">Số lượng: 2</span>
 									</li>
-							</ul>
+									<li class="row prodcut-item">
+											<img class="col-sm-4 product-img" alt="hình ảnh">
+											<span class="col-sm-5 item-padding">Thông tin sản phẩm</span>
+											<span class="col-sm-3 item-padding">Số lượng: 1</span>
+										</li>
+								</ul>
+							</div>
 						</div>
 					</div>
-					<hr>
-					<div class="row item">
-						<div class="col-sm-4 item-info">
-							<p>Đơn hàng <strong>#542</strong></p>
-							<p>Ngày đặt: 09/2/2018</p>
-							<p>Giá: 10000000Đ</p>
-						</div>
-						<div class="col-sm-8 item-detail">
-							<ul class="product-menu">
-								<li class="prodcut-item">
-										<img alt="hình ảnh">
-										<span>Thông tin sản phẩm</span>
-										<span>Số lượng: 1</span>
-									</li>
-							</ul>
-						</div>
-					</div>
+					@endforeach
 				</div>
       </section>
     </div>
