361a991a0472bf3cdb1e609324d5f880d3415af4
thanhbuiv <thanh.bui@asiantech.vn>
2019-03-21 16:45:34 +0700

    handle delete review

--- app/Services/ReviewService.php
+++ app/Services/ReviewService.php
@@ -3,6 +3,8 @@
 namespace App\Services;
 
 use App\Models\Review;
+use Log;
+use File;
 
 class ReviewService
 {
@@ -14,5 +16,29 @@ class ReviewService
     public function getReviewWithPaginate()
     {
         return Review::with(['user:id', 'user.profile:id,user_id,name', 'product:id,name', 'images'])->orderBy('id', 'desc')->paginate(config('define.paginate.limit_rows'));
+    }
+
+    /**
+     * Remove review
+     *
+     * @param Review $review review
+     *
+     * @return boolean
+     */
+    public function destroy(Review $review)
+    {
+        try {
+            $review->delete();
+            if (!$review->images->isEmpty()) {
+                foreach ($review->images as $image) {
+                    $image->delete();
+                    File::delete(public_path($image->path));
+                }
+            }
+            return $review;
+        } catch (\Exception $e) {
+            Log::error($e);
+            return false;
+        }
     }
 }
