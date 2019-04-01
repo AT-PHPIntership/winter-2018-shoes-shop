75570d06ab4837c58b14d454b19f567dd193d1a0
thanhbuiv <thanh.bui@asiantech.vn>
2019-04-01 15:48:12 +0700

    change total_review, avg_rating after change status

--- app/Services/ReviewService.php
+++ app/Services/ReviewService.php
@@ -3,7 +3,9 @@
 namespace App\Services;
 
 use App\Models\Review;
+use App\Models\Product;
 use Log;
+use DB;
 
 class ReviewService
 {
@@ -27,11 +29,31 @@ class ReviewService
      */
     public function changeStatus(int $status, int $id)
     {
+        DB::beginTransaction();
         try {
-            return Review::where('id', $id)->update(['status' => $status]);
+            $review = Review::find($id);
+            if ($review->status == $status) {
+                return false;
+            }
+            $product = Product::find($review->product_id);
+            if ($status == Review::ACTIVE_STATUS) {
+                $newTotalReview = $product->total_review + 1;
+                $newAvgRating = ($product->total_review * $product->avg_rating + $review->star) / $newTotalReview;
+            } else {
+                $newTotalReview = $product->total_review -1;
+                $newAvgRating = ($product->total_review * $product->avg_rating - $review->star) / $newTotalReview;
+            }
+            $product->update([
+                'total_review' => $newTotalReview,
+                'avg_rating' => $newAvgRating,
+            ]);
+            $review = Review::where('id', $id)->update(['status' => $status]);
+            DB::commit();
+            return $review;
         } catch (\Exception $e) {
             Log::error($e);
-            return false;
+            DB::rollback();
         }
+        return false;
     }
 }
